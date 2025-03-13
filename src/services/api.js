17ed8e758a290991f64ce2bc50310027173b201b
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-03-12 22:37:34 -0400

    api methods

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,16 @@
+//import md5 = require("md5")
+
 const API_BASE_URL = import.meta.env.API_BASE_URL
+const API_MARVEL_PUBLIC_KEY = import.meta.env.API_MARVEL_PUBLIC_KEY
+const API_MARVEL_PRIVATE_KEY = import.meta.env.API_MARVEL_PRIVATE_KEY
+
+const COMICS_ENTITY_QUERY = "comics"
+const CHARACTERS_ENTITY_QUERY = "characters"
+
+const ts = Date.now()
+const hashInput = ts + API_MARVEL_PRIVATE_KEY + API_MARVEL_PUBLIC_KEY
+const hash = Crypto.MD5(hashInput).toString()
+
 
 // arrow function for handleResponse
 // async required when using await
@@ -12,10 +24,20 @@ const handleResponse = async (response) => {
     }
     // got back response
     return response.json()
-}
+};
 
-const request = async (endpoint, options = {}) => {
-    const defaultOptions = {
+// request using endpoint
+const request = async(endpoint) => {
 
+    try {
+        // response will be the fetch using url and enpoint
+        const response = await fetch(`${API_BASE_URL}${endpoint}`)
+        // use handleResponse to take the response and return json
+        return await handleResponse(response)
+    } catch(error) {
+        console.error('API request failed:', error)
+        throw error
     }
 }
+
+// API Methods
