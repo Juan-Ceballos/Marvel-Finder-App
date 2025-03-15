a9ba4f32cc57798f47422794dba28984c05ac78f
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-03-15 18:52:38 -0400

    removed more template, added default export that shows basic outlining and imported to root

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import ProfileContainer from './Components/MarvelCharacterProfile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <ProfileContainer />
     </>
   )
 }
---
+++ src/Components/MarvelCharacterProfile.jsx
@@ -0,0 +1,24 @@
+function MarvelCharacterTN() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+function MarvelNameLabel() {
+    return (
+        <div>
+            <p>Name Here</p>
+        </div>
+    )
+}
+
+export default function ProfileContainer() {
+    return (
+        <>
+            <MarvelCharacterTN></MarvelCharacterTN>
+            <MarvelNameLabel></MarvelNameLabel>
+        </>
+    )
+}
