9abefd1a93306cfd0bd2f474d7fa03cc6520c2ce
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-03-22 16:18:54 -0400

    added error messages based on error properties

--- src/services/error-service.js
+++ src/services/error-service.js
@@ -27,19 +27,37 @@ const ErrorService = {
          }
 
          return new ResponseError(
-            endpoint
-            statusCode
-            statusText
+            endpoint,
+            statusCode,
+            statusText,
             {
                 message: error.message || "Unknown Error",
                 type: errorType,
                 clientSide: !Error.statusScode
             }
          )
-    }
+    },
 
     getUserFriendlyMessage: (error) => {
-
+        if (error.details.clientSide) {
+            if (error.statusCode === 0) {
+                return "Please check your internet connection and try again."
+            } else if (error.statusCode === -1) {
+                return "We received an invalid response from the server."
+            } else {
+                return "Something went wrong in the application. Please try again."
+            }
+        } else {
+            if ((error.statusCode === 401) || error.statusCode === 403) {
+                return "You don't have permission to access this resource."
+            } else if (error.statusCode === 404) {
+                return "The requested information could not be found."
+            } else if (error.statusCode >= 500) {
+                return "The server encountered a problem. Please try again later."
+            } else {
+                return "An error occurred while processing your request."
+            }
+        }
     }
 
 }
